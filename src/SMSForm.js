4a14e6a35b065edd4ec778360e962a0d0224aea1
Phil Nash <philnash@gmail.com>
2020-11-24 10:26:51 +1100

    Turns SMSForm to function component with useState and useCallback

--- src/SMSForm.js
+++ src/SMSForm.js
@@ -1,89 +1,62 @@
-import React, { Component } from 'react';
-import './SMSForm.css';
+import React, { useState, useCallback } from "react";
+import "./SMSForm.css";
 
-class SMSForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      message: {
-        to: '',
-        body: ''
-      },
-      submitting: false,
-      error: false
-    };
-    this.onSubmit = this.onSubmit.bind(this);
-    this.onHandleChange = this.onHandleChange.bind(this);
-  }
+const SMSForm = () => {
+  const [to, setTo] = useState("");
+  const [body, setBody] = useState("");
+  const [submitting, setSubmitting] = useState(false);
+  const [error, setError] = useState(false);
 
-  onSubmit(event) {
-    event.preventDefault();
-    this.setState({ submitting: true });
-    fetch('/api/messages', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(this.state.message)
-    })
-      .then(res => res.json())
-      .then(data => {
-        if (data.success) {
-          this.setState({
-            error: false,
-            submitting: false,
-            message: {
-              to: '',
-              body: ''
-            }
-          });
-        } else {
-          this.setState({
-            error: true,
-            submitting: false
-          });
-        }
-      });
-  }
+  const onToChange = useCallback((event) => {
+    setTo(event.target.value);
+  }, []);
 
-  onHandleChange(event) {
-    const name = event.target.getAttribute('name');
-    this.setState({
-      message: { ...this.state.message, [name]: event.target.value }
-    });
-  }
+  const onBodyChange = useCallback((event) => {
+    setBody(event.target.value);
+  }, []);
 
-  render() {
-    return (
-      <form
-        onSubmit={this.onSubmit}
-        className={this.state.error ? 'error sms-form' : 'sms-form'}
-      >
-        <div>
-          <label htmlFor="to">To:</label>
-          <input
-            type="tel"
-            name="to"
-            id="to"
-            value={this.state.message.to}
-            onChange={this.onHandleChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="body">Body:</label>
-          <textarea
-            name="body"
-            id="body"
-            value={this.state.message.body}
-            onChange={this.onHandleChange}
-          />
-        </div>
-        <button type="submit" disabled={this.state.submitting}>
-          Send message
-        </button>
-      </form>
-    );
-  }
-}
+  const onSubmit = useCallback(
+    (event) => {
+      event.preventDefault();
+      setSubmitting(true);
+      fetch("/api/messages", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ to, body }),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.success) {
+            setError(false);
+            setSubmitting(false);
+            setTo("");
+            setBody("");
+          } else {
+            setError(true);
+            setSubmitting(false);
+          }
+        });
+    },
+    [to, body]
+  );
+
+  return (
+    <form onSubmit={onSubmit} className={error ? "error sms-form" : "sms-form"}>
+      <div>
+        <label htmlFor="to">To:</label>
+        <input type="tel" name="to" id="to" value={to} onChange={onToChange} />
+      </div>
+      <div>
+        <label htmlFor="body">Body:</label>
+        <textarea name="body" id="body" value={body} onChange={onBodyChange} />
+      </div>
+      <button type="submit" disabled={submitting}>
+        Send message
+      </button>
+    </form>
+  );
+};
 
 export default SMSForm;
