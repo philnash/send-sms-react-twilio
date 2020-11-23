972e34d0266493564c8d125c0039d48c7470c2e7
Phil Nash <philnash@gmail.com>
2020-11-24 10:18:05 +1100

    Turns App to function component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import SMSForm from './SMSForm';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import SMSForm from "./SMSForm";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-
-          <SMSForm />
-        </header>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <SMSForm />
+      </header>
+    </div>
+  );
+};
 
 export default App;
